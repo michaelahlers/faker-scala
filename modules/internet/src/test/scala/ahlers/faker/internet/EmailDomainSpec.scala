0e2769be89f86676573107da24e76f5301fad4a0
Michael Ahlers <michael@ahlers.consulting>
2020-05-06 21:46:01 -0400

    Test constructor

---
+++ modules/internet/src/test/scala/ahlers/faker/internet/EmailDomainSpec.scala
@@ -0,0 +1,25 @@
+package ahlers.faker.internet
+
+import eu.timepit.refined.auto._
+import org.scalatest.wordspec._
+import org.scalatest.matchers.should.Matchers._
+
+/**
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ * @since May 06, 2020
+ */
+class EmailDomainSpec extends AnyWordSpec {
+
+  "constructor" in {
+
+    """EmailDomain("example.com")""".should(compile)
+    """EmailDomain("[192.168.1.1]")""".should(compile)
+    """EmailDomain("[::1:1]")""".should(compile)
+
+    """EmailDomain("")""".shouldNot(compile)
+    """EmailDomain("[192.168.1.1")""".shouldNot(compile)
+    """EmailDomain("[::1:1")""".shouldNot(compile)
+
+  }
+
+}
