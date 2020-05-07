7aebf1b994b417581a48e0c814e4e50ddaed0f12
Michael Ahlers <michael@ahlers.consulting>
2020-05-06 21:32:50 -0400

    Use recently-added refinements

--- modules/internet/src/main/scala/ahlers/faker/internet/package.scala
+++ modules/internet/src/main/scala/ahlers/faker/internet/package.scala
@@ -1,11 +1,8 @@
 package ahlers.faker
 
-import ahlers.faker.refined.extras
+import ahlers.refined.string._
 import eu.timepit.refined.api._
-import eu.timepit.refined.auto._
 import eu.timepit.refined.boolean._
-import eu.timepit.refined.collection
-import eu.timepit.refined.string
 import io.estatico.newtype.macros._
 
 /**
@@ -15,22 +12,16 @@ import io.estatico.newtype.macros._
 package object internet {
 
   /** @see [[https://emailregex.com]] */
-  type EmailLocalType = String Refined
-    (collection.NonEmpty
-      And string.MatchesRegex["""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")"""]
-      And collection.MaxSize[64]
-      And string.Trimmed)
+  type EmailLocalType = String Refined MatchEmailLocal
 
   /** @see [[https://en.wikipedia.org/wiki/Email_address#Local-part]] */
   @newtype case class EmailLocal(toText: EmailLocalType)
 
   /** @see [[https://emailregex.com]] */
   type EmailDomainType = String Refined
-    (collection.MaxSize[255]
-      And string.Trimmed
-      And (string.MatchesRegex["""(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"""]
-        Or extras.Drop[1, 1, string.IPv4]
-        Or extras.Drop[1, 1, string.IPv6]))
+    (MatchEmailDomainHost
+      Or MatchEmailDomainIPv4
+      Or MatchEmailDomainIPv6)
 
   /** @see [[https://en.wikipedia.org/wiki/Email_address#Domain]] */
   @newtype case class EmailDomain(toText: EmailDomainType)
