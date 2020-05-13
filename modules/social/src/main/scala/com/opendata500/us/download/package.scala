b20975a8ab435e4194bdd4f6dc8eb8617e7e79c7
Michael Ahlers <michael.ahlers@livesafemobile.com>
2020-05-13 09:42:02 -0400

    Factor shared instances

--- modules/social/src/main/scala/com/opendata500/us/download/package.scala
+++ modules/social/src/main/scala/com/opendata500/us/download/package.scala
@@ -1,20 +1,16 @@
 package com.opendata500.us
 
 import ahlers.faker.social._
+import com.opendata500.kantan._
 import kantan.csv._
 import kantan.csv.ops._
 import kantan.csv.generic._
-import kantan.csv.refined._
 
 /**
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  * @since May 11, 2020
  */
 package object download {
-
-  implicit private val CellDecoderCompanyId: CellDecoder[CompanyId] = CompanyId.deriving
-  implicit private val CellDecoderCompanyName: CellDecoder[CompanyName] = CompanyName.deriving
-  implicit private val CellDecoderCompanyWebsite: CellDecoder[Seq[CompanyWebsite]] = CompanyWebsite.deriving[CellDecoder].map(Seq(_))
 
   val usCompanies: IndexedSeq[Company] =
     Thread.currentThread()
