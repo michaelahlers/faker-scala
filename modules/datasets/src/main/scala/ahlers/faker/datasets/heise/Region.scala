999720b2c08c736862edfcc4d074e412be67bc10
Michael Ahlers <michael@ahlers.consulting>
2021-08-28 17:19:45 -0600

    Sort alphabetically

--- modules/datasets/src/main/scala/ahlers/faker/datasets/heise/Region.scala
+++ modules/datasets/src/main/scala/ahlers/faker/datasets/heise/Region.scala
@@ -7,59 +7,59 @@ package ahlers.faker.datasets.heise
 sealed trait Region
 object Regions {
 
-  case object `Great Britain` extends Region
-  case object `Ireland` extends Region
-  case object `United States` extends Region
-  case object `Italy` extends Region
-  case object `Malta` extends Region
-  case object `Portugal` extends Region
-  case object `Spain` extends Region
-  case object `France` extends Region
-  case object `Belgium` extends Region
-  case object `Luxembourg` extends Region
-  case object `Netherlands` extends Region
-  case object `East Frisia` extends Region
-  case object `Germany` extends Region
+  case object `Albania` extends Region
+  case object `Arabia/Persia` extends Region
+  case object `Armenia` extends Region
   case object `Austria` extends Region
-  case object `Switzerland` extends Region
-  case object `Iceland` extends Region
+  case object `Azerbaijan` extends Region
+  case object `Belarus` extends Region
+  case object `Belgium` extends Region
+  case object `Bosnia/Herzegovina` extends Region
+  case object `Bulgaria` extends Region
+  case object `China` extends Region
+  case object `Croatia` extends Region
+  case object `Czech Republic` extends Region
   case object `Denmark` extends Region
-  case object `Norway` extends Region
-  case object `Sweden` extends Region
-  case object `Finland` extends Region
+  case object `East Frisia` extends Region
   case object `Estonia` extends Region
+  case object `Finland` extends Region
+  case object `France` extends Region
+  case object `Georgia` extends Region
+  case object `Germany` extends Region
+  case object `Great Britain` extends Region
+  case object `Greece` extends Region
+  case object `Hungary` extends Region
+  case object `Iceland` extends Region
+  case object `India/Sri Lanka` extends Region
+  case object `Ireland` extends Region
+  case object `Israel` extends Region
+  case object `Italy` extends Region
+  case object `Japan` extends Region
+  case object `Kazakhstan/Uzbekistan` extends Region
+  case object `Korea` extends Region
+  case object `Kosovo` extends Region
   case object `Latvia` extends Region
   case object `Lithuania` extends Region
-  case object `Poland` extends Region
-  case object `Czech Republic` extends Region
-  case object `Slovakia` extends Region
-  case object `Hungary` extends Region
-  case object `Romania` extends Region
-  case object `Bulgaria` extends Region
-  case object `Bosnia/Herzegovina` extends Region
-  case object `Croatia` extends Region
-  case object `Kosovo` extends Region
+  case object `Luxembourg` extends Region
   case object `Macedonia` extends Region
-  case object `Montenegro` extends Region
-  case object `Serbia` extends Region
-  case object `Slovenia` extends Region
-  case object `Albania` extends Region
-  case object `Greece` extends Region
-  case object `Russia` extends Region
-  case object `Belarus` extends Region
+  case object `Malta` extends Region
   case object `Moldova` extends Region
-  case object `Ukraine` extends Region
-  case object `Armenia` extends Region
-  case object `Azerbaijan` extends Region
-  case object `Georgia` extends Region
-  case object `Kazakhstan/Uzbekistan` extends Region
+  case object `Montenegro` extends Region
+  case object `Netherlands` extends Region
+  case object `Norway` extends Region
+  case object `Poland` extends Region
+  case object `Portugal` extends Region
+  case object `Romania` extends Region
+  case object `Russia` extends Region
+  case object `Serbia` extends Region
+  case object `Slovakia` extends Region
+  case object `Slovenia` extends Region
+  case object `Spain` extends Region
+  case object `Sweden` extends Region
+  case object `Switzerland` extends Region
   case object `Turkey` extends Region
-  case object `Arabia/Persia` extends Region
-  case object `Israel` extends Region
-  case object `China` extends Region
-  case object `India/Sri Lanka` extends Region
-  case object `Japan` extends Region
-  case object `Korea` extends Region
+  case object `Ukraine` extends Region
+  case object `United States` extends Region
   case object `Vietnam` extends Region
 
   case object `Other` extends Region
