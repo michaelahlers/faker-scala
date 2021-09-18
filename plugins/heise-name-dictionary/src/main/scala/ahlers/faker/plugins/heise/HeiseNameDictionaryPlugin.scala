136073a0fec6caac085b3cff9c02343bdb021b98
Michael Ahlers <michael@ahlers.consulting>
2021-09-17 19:59:41 -0600

    Source directly from the archive without distributing

--- plugins/heise-name-dictionary/src/main/scala/ahlers/faker/plugins/heise/HeiseNameDictionaryPlugin.scala
+++ plugins/heise-name-dictionary/src/main/scala/ahlers/faker/plugins/heise/HeiseNameDictionaryPlugin.scala
@@ -2,68 +2,83 @@ package ahlers.faker.plugins.heise
 
 import sbt.Keys._
 import sbt._
-import better.files._
 
-import java.io.InputStream
-import java.nio.file.Path
-import java.util.zip.ZipInputStream
-import scala.io.Codec
-import scala.io.Source
+import java.nio.charset.StandardCharsets
+import scala.collection.convert.ImplicitConversionsToScala._
 
 object HeiseNameDictionaryPlugin extends AutoPlugin {
 
-  case class CharacterEncoding(pattern: String, substitution: String)
-
-  def linesF(archive: ZipInputStream): Dispose[Iterator[String]] = {
-    while (!(archive.getNextEntry().getName() == "0717-182/nam_dict.txt")) {}
-
-    Source
-      .fromInputStream(archive)(Codec.ISO8859)
-      .autoClosed
-      .map(_.getLines())
-  }
-
-  def linesF(resource: Path): Dispose[Iterator[String]] =
-    File(resource)
-      .newZipInputStream
-      .autoClosed
-      .flatMap(linesF(_))
-
-  def linesF(): Dispose[Iterator[String]] =
-    Resource
-      .getAsStream("ftp.heise.de/pub/ct/listings/0717-182.zip")
-      .asZipInputStream()
-      .autoClosed
-      .flatMap(linesF(_))
-
   object autoImport {
-    //val helloGreeting = settingKey[String]("greeting")
+
+    val heiseNameDictionaryArchiveSourceUrl = settingKey[URL]("greeting")
+
+    val heiseNameDictionaryDownloadDirectory = settingKey[File]("greeting")
+
+    val heiseNameDictionaryFileName = settingKey[String]("greeting")
+
+    val downloadHeiseNameDictionaryFile = taskKey[File]("")
+
     val hello = taskKey[Unit]("say hello")
+
   }
 
   import autoImport._
 
-  //override val globalSettings =
-  Seq(
-    //helloGreeting := "hi"
-  )
-
-  override val projectSettings =
+  override val globalSettings =
     Seq(
-      hello := {
-        val log = streams.value.log
-
-        linesF(((Compile / resourceDirectory).value /
+      heiseNameDictionaryArchiveSourceUrl :=
+        url("ftp://ftp.heise.de/pub/ct/listings/0717-182.zip"),
+      heiseNameDictionaryDownloadDirectory :=
+        taskTemporaryDirectory.value /
           "ftp.heise.de" /
           "pub" /
           "ct" /
-          "listings" /
-          "0717-182.zip")
-          .toPath) { lines =>
-          lines
-            .take(10)
-            .foreach(log.info(_))
-        }
+          "listings",
+      heiseNameDictionaryFileName := "nam_dict.txt"
+    )
+
+  override val projectSettings =
+    Seq(
+      downloadHeiseNameDictionaryFile := {
+        val log = streams.value.log
+
+        val sourceUrl = heiseNameDictionaryArchiveSourceUrl.value
+        val downloadDirectory = heiseNameDictionaryDownloadDirectory.value
+
+        val downloadFiles =
+          IO.unzipURL(
+            sourceUrl,
+            downloadDirectory)
+
+        log.info(s"""Extracted ${downloadFiles.size} files from archive "$sourceUrl" to "$downloadDirectory":""")
+
+        downloadFiles
+          .map(_
+            .toPath
+            .drop(downloadDirectory
+              .toPath
+              .size)
+            .reduce(_.resolve(_))
+            .toString)
+          .foreach(log.info(_))
+
+        val dictionaryFileName = heiseNameDictionaryFileName.value
+        val dictionaryFile =
+          downloadFiles
+            .find(_.getName() == dictionaryFileName)
+            .getOrElse(throw new MessageOnlyException(s"""File "$dictionaryFileName" wasn't found in archive "$sourceUrl"."""))
+
+        log.info(s"""Heise name dictionary saved to "$dictionaryFile".""")
+
+        dictionaryFile
+      },
+      hello := {
+        val log = streams.value.log
+        val dictionaryFile = downloadHeiseNameDictionaryFile.value
+
+        IO.readLines(dictionaryFile, StandardCharsets.ISO_8859_1)
+          .take(10)
+          .foreach(log.info(_))
 
       }
     )
