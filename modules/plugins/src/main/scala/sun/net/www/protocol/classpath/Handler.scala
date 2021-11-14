fca8d1f3a1f472949277732866cecef18afb57c0
Michael Ahlers <michael@ahlers.consulting>
2021-11-14 14:40:28 -0700

    Move to standard package

--- modules/plugins/src/main/scala/sun/net/www/protocol/classpath/Handler.scala
+++ modules/plugins/src/main/scala/sun/net/www/protocol/classpath/Handler.scala
@@ -1,18 +1,22 @@
-package ahlers.faker.plugins
+package sun.net.www.protocol.classpath
 
 import java.net.URL
 import java.net.URLStreamHandler
 
 /**
  * Supports opening connections with `classpath` as a URL scheme. Adapted to Scala from [[https://stackoverflow.com/a/1769454 an answer in Java on Stack Overflow]]. Licensed to the public domain by its original author.
+ *
  * @see [[https://stackoverflow.com/q/861500 URL to load resources from the classpath in Java]]
  * @see [[https://stackoverflow.com/users/37193/stephen]]
+ *
  * @since November 20, 2009
  */
-object ClassPathStreamHandler extends URLStreamHandler {
+class Handler extends URLStreamHandler {
   override def openConnection(location: URL) =
     getClass
       .getClassLoader
       .getResource(location.getPath)
       .openConnection()
 }
+
+object Handler extends Handler
