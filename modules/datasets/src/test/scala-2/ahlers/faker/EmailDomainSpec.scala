7c2ff74ec377862c35bc97267c04f3f7562d33dd
Michael Ahlers <michael@ahlers.consulting>
2022-01-01 11:41:59 -0700

    Only test for Scala 2

--- modules/datasets/src/test/scala-2/ahlers/faker/EmailDomainSpec.scala
+++ modules/datasets/src/test/scala-2/ahlers/faker/EmailDomainSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec._
 
 /**
+ * @todo Known to fail with Scala 3; restore for all versions if [[https://github.com/fthomas/refined/issues/932]] is resolved.
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  * @since May 06, 2020
  */
